97f5a88527a2c9dfdbe8aaef03c0178d60b6ce1b
surya prakash <155574828+storycapui@users.noreply.github.com>
2025-05-03 14:39:37 +0530

    commit changes
    removed webvitals and updated indexjs file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { sendToVercelAnalytics } from './vitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals(sendToVercelAnalytics);
+
